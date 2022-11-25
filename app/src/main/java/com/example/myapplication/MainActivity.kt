dedeb23b7714a5e847c2086953eca849bfec7290
RudolfKitler <togoll60@gmail.com>
2022-11-25 19:08:14 +1000

    java top

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,11 +7,17 @@ import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
 import com.github.aachartmodel.aainfographics.aachartcreator.AAChartView
 import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
 
+data class Data(var date: String = "",
+                var time: String = "",
+                var temperature: Double = 0.0,
+                var humidity: Double = 0.0)
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val aaChartView = findViewById<AAChartView>(R.id.aa_chart_view)
+
+       /* val aaChartView = findViewById<AAChartView>(R.id.aa_chart_view)
         val aaChartModel : AAChartModel = AAChartModel()
             .chartType(AAChartType.Area)
             //.title("title")
@@ -22,14 +28,14 @@ class MainActivity : AppCompatActivity() {
                 AASeriesElement()
                     .name("Температура")
                     .data(arrayOf(7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6)),
-               // AASeriesElement()
-               //    .name("Влажность")
-               //    .data(arrayOf(0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5)),
+                AASeriesElement()
+                   .name("Влажность")
+                   .data(arrayOf(0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5)),
 
             )
             )
         aaChartView.aa_drawChartWithChartModel(aaChartModel)
-
+*/
         val aaChartView2 = findViewById<AAChartView>(R.id.aa_chart_view2)
         val aaChartModel2 : AAChartModel = AAChartModel()
             .chartType(AAChartType.Area)
