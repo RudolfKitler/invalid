78082783b4476a900c246d50ba5196152a37df1a
RudolfKitler <togoll60@gmail.com>
2022-11-25 18:53:47 +1000

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartView
+import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val aaChartView = findViewById<AAChartView>(R.id.aa_chart_view)
+        val aaChartModel : AAChartModel = AAChartModel()
+            .chartType(AAChartType.Area)
+            //.title("title")
+            //.subtitle("subtitle")
+            .backgroundColor("#ffffff")
+            .dataLabelsEnabled(true)
+            .series(arrayOf(
+                AASeriesElement()
+                    .name("Температура")
+                    .data(arrayOf(7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6)),
+               // AASeriesElement()
+               //    .name("Влажность")
+               //    .data(arrayOf(0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5)),
+
+            )
+            )
+        aaChartView.aa_drawChartWithChartModel(aaChartModel)
+
+        val aaChartView2 = findViewById<AAChartView>(R.id.aa_chart_view2)
+        val aaChartModel2 : AAChartModel = AAChartModel()
+            .chartType(AAChartType.Area)
+            .title("Ааааааааа")
+            //.subtitle("subtitle")
+            .backgroundColor("#ffffff")
+            .dataLabelsEnabled(true)
+            .series(arrayOf(
+                AASeriesElement()
+                    .name("Температура")
+                    .data(arrayOf(7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6)),
+                AASeriesElement()
+                    .name("Влажность")
+                    .data(arrayOf(0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5)),
+
+                )
+            )
+        aaChartView2.aa_drawChartWithChartModel(aaChartModel2)
+
+
+    }
+}
